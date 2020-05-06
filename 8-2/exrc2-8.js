799a3bcaa6bb7e98ab99a3fb6efcdb179a9787f7
danilossousa <danilosantossousa@gmail.com>
2020-05-06 05:50:55 -0300

    Middle commit

--- 8-2/exrc2-8.js
+++ 8-2/exrc2-8.js
@@ -20,10 +20,7 @@ const lesson3 = {
 
 console.log(
   ((o, k, v) => {
-    const aEntries = Object.entries(o);
-    for (let i = 0; i < aEntries.length; i += 1) {
-      if (aEntries[i][0] === k && aEntries[i][1] === v) return true;
-    }
+    if (o[k] === v) return true;
     return false;
-  })(lesson3, 'turno', 'noite')
+  })(lesson1, 'turno', 'manhã')
 );
