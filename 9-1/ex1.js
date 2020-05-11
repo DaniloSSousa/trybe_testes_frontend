e44a57d9a19ef5da85f1869ecb43290f5de8ae57
danilossousa <danilosantossousa@gmail.com>
2020-05-11 15:59:57 -0300

    Init commit

--- 9-1/ex1.js
+++ 9-1/ex1.js
@@ -64,7 +64,7 @@ const books = [
 ];
 
 function authorBornIn1947() {
-  const book = books.find((book) => book.author.birthYear === 1947);
+  const book = books.find(book => book.author.birthYear === 1947);
   return book.author.name;
 }
 
